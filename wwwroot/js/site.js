b47d76b6ffcd705c957f42b96681173e4ba0854a
is <66485860+kasairyujin@users.noreply.github.com>
2022-11-27 16:11:41 -0500

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,18 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const setTheme = (theme) => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+}
+
+const getTheme = () => {
+    const theme = localStorage.getItem('theme');
+    theme && setTheme(theme);
+}
+
+getTheme();
+document.getElementById('theme-select').addEventListener('change', function () {
+    setTheme(this.value);
+});
